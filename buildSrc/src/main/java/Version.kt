0ccd8197bba95ec7f31d79712306a101ac29bb3a
유현명 <aahspringaa4@naver.com>
2022-06-26 20:55:08 +0900

    ⚡️ :: Compose Version

---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,8 @@
+object Version {
+    const val NAV = "2.4.1"
+    const val ANI_NAV = "0.24.5-alpha"
+    const val LANDSCAPIST = "1.4.7"
+
+    const val COMPOSE = "1.2.0-alpha03"
+    const val ACTIVITY_COMPOSE = "1.4.0"
+}
