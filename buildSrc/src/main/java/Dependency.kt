def111f19b59a28f26a4f0325be1278650ed6c25
유현명 <aahspringaa4@naver.com>
2022-06-26 20:54:57 +0900

    ⚡️ :: Kotlin DSL Dependency

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,13 @@
+object Dependency {
+    object Compose {
+        const val COMPOSE_UI = "androidx.compose.ui:ui:${Version.COMPOSE}"
+        const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Version.COMPOSE}"
+        const val COMPOSE_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${Version.COMPOSE}"
+        const val COMPOSE_ACTIVITY = "androidx.activity:activity-compose:${Version.ACTIVITY_COMPOSE}"
+        const val COMPOSE_TEST = "androidx.compose.ui:ui-test-junit4:${Version.COMPOSE}"
+        const val COMPOSE_UI_TOOL = "androidx.compose.ui:ui-tooling:${Version.COMPOSE}"
+        const val COMPOSE_NAV = "androidx.navigation:navigation-compose:${Version.NAV}"
+        const val COMPOSE_ANI_NAV = "com.google.accompanist:accompanist-navigation-animation:${Version.ANI_NAV}"
+        const val COMPOSE_LANDSCAPIST = "com.github.skydoves:landscapist-glide:${Version.LANDSCAPIST}"
+    }
+}
