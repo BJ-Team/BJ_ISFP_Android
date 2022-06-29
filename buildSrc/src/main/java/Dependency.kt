6cb56655c32a671a8b8922c9451558d7006b4eea
유현명 <aahspringaa4@naver.com>
2022-06-29 21:26:03 +0900

    ⚡️ :: Dependency Update

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,13 +1,34 @@
 object Dependency {
-    object Compose {
-        const val COMPOSE_UI = "androidx.compose.ui:ui:${Version.COMPOSE}"
-        const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Version.COMPOSE}"
-        const val COMPOSE_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${Version.COMPOSE}"
-        const val COMPOSE_ACTIVITY = "androidx.activity:activity-compose:${Version.ACTIVITY_COMPOSE}"
-        const val COMPOSE_TEST = "androidx.compose.ui:ui-test-junit4:${Version.COMPOSE}"
-        const val COMPOSE_UI_TOOL = "androidx.compose.ui:ui-tooling:${Version.COMPOSE}"
-        const val COMPOSE_NAV = "androidx.navigation:navigation-compose:${Version.NAV}"
-        const val COMPOSE_ANI_NAV = "com.google.accompanist:accompanist-navigation-animation:${Version.ANI_NAV}"
-        const val COMPOSE_LANDSCAPIST = "com.github.skydoves:landscapist-glide:${Version.LANDSCAPIST}"
+
+    const val appcompat = "androidx.appcompat:appcompat:${Version.appcompat}"
+    const val coreKtx = "androidx.core:core-ktx:${Version.coreKtx}"
+    const val androidKtx = "androidx.activity:activity-ktx:${Version.androidKtx}"
+    const val fragmentKtx = "androidx.fragment:fragment-ktx:${Version.fragmentKtx}"
+
+    object UI {
+        const val composeUi = "androidx.compose.ui:ui:${Version.jetpackCompose}"
+        const val composeMaterial = "androidx.compose.material:material:${Version.jetpackCompose}"
+        const val composePreView = "androidx.compose.ui:ui-tooling-preview:${Version.jetpackCompose}"
+        const val composeActivity =
+            "androidx.activity:activity-compose:${Version.activityCompose}"
+        const val composeTest = "androidx.compose.ui:ui-test-junit4:${Version.jetpackCompose}"
+        const val composeUiTool = "androidx.compose.ui:ui-tooling:${Version.jetpackCompose}"
+
+        const val composeNav = "androidx.navigation:navigation-compose:${Version.nav}"
+        const val composeAniNav =
+            "com.google.accompanist:accompanist-navigation-animation:${Version.aniNav}"
+    }
+
+    object Lifecycle {
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.lifecycle}"
+        const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Version.lifecycle}"
+        const val runTime = "androidx.lifecycle:lifecycle-runtime-ktx:${Version.lifecycle}"
+    }
+
+    object GradlePlugin {
+        const val android = "com.android.tools.build:gradle:${Version.gradle}"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.kotlin}"
+        const val hilt = "com.google.dagger:hilt-android-gradle-plugin:${Version.hilt}"
+        const val service = "com.google.gms:google-services:${Version.service}"
     }
 }
