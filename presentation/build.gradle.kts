b49cee5a944caa55f46c1e0162d0a0941908dedd
유현명 <aahspringaa4@naver.com>
2022-07-04 10:42:22 +0900

    ⚙️ :: Update Gradle

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -27,13 +27,12 @@ android {
         targetCompatibility = Project.javaVersion
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = Version.COMPOSE
+        kotlinCompilerExtensionVersion = Version.jetpackCompose
     }
     kotlinOptions {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        dataBinding = true
         compose = true
     }
 }
@@ -44,63 +43,21 @@ dependencies {
     implementation(Dependency.appcompat)
     implementation(Dependency.androidKtx)
     implementation(Dependency.fragmentKtx)
+    implementation(Dependency.material)
 
-    implementation(Dependency.UI.composeActivity)
-    implementation(Dependency.UI.composeMaterial)
-    implementation(Dependency.UI.composePreView)
-    implementation(Dependency.UI.composeTest)
-    implementation(Dependency.UI.composeUi)
-    implementation(Dependency.UI.composeUiTool)
-    implementation(Dependency.UI.composeActivity)
-    implementation(Dependency.UI.composeNav)
-    implementation(Dependency.UI.composeAniNav)
+    implementation(Dependency.UI.Compose.composeActivity)
+    implementation(Dependency.UI.Compose.composeMaterial)
+    implementation(Dependency.UI.Compose.composePreView)
+    implementation(Dependency.UI.Compose.composeTest)
+    implementation(Dependency.UI.Compose.composeUi)
+    implementation(Dependency.UI.Compose.composeUiTool)
+    implementation(Dependency.UI.Compose.composeActivity)
+    implementation(Dependency.UI.Compose.composeNav)
+    implementation(Dependency.UI.Compose.composeAniNav)
 
-    testImplementation(Dependency.Test.junit)
-    testImplementation(Dependency.Test.mockito)
-    androidTestImplementation(Dependency.Test.androidJunit)
-    androidTestImplementation(Dependency.Test.espresso)
-
-    implementation(Dependency.DI.hiltAndroid)
-    implementation(Dependency.DI.hiltCompose)
-    kapt(Dependency.DI.hiltCompiler)
-
-    implementation(Dependency.Network.retrofit)
-    implementation(Dependency.Network.gsonConverter)
-    implementation(Dependency.Network.okhttp)
-    implementation(Dependency.Network.loggingInterceptor)
-
-    implementation(Dependency.LocalStorage.room)
-    kapt(Dependency.LocalStorage.roomCompiler)
-
-    implementation(Dependency.Coroutine.core)
-    implementation(Dependency.Coroutine.android)
+    implementation(Dependency.UI.ViewModel.viewModel)
 
     implementation(Dependency.Lifecycle.viewModel)
     implementation(Dependency.Lifecycle.liveData)
     implementation(Dependency.Lifecycle.runTime)
-
-    implementation(Dependency.Navigation.navigationFragment)
-    implementation(Dependency.Navigation.navigationUi)
-
-    implementation(Dependency.WorkManager.ktx)
-    implementation(Dependency.WorkManager.hiltExtension)
-
-    implementation(Dependency.FireBase.fcm)
-    implementation(Dependency.FireBase.message)
-
-    implementation(Dependency.GooglePlayService.fitness)
-    implementation(Dependency.GooglePlayService.auth)
-
-    implementation(Dependency.Permission.tedPermission)
-
-    implementation(Dependency.ThreeTenAndroidBackport.threeTenAbp)
-
-    implementation(Dependency.CircleImageView.circleImage)
-
-    implementation(Dependency.Glide.glideCore)
-    annotationProcessor(Dependency.Glide.glideCompiler)
-
-    implementation(Dependency.Socket.socketIo) {
-        exclude(group = "org.json", module = "json")
-    }
 }
