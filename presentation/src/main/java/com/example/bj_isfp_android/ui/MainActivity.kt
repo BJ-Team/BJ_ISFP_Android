ee17a6c132119a6adb389f0137a6a2225bff3645
유현명 <aahspringaa4@naver.com>
2022-07-07 10:19:55 +0900

    ⚡️ :: MainActivity Method

--- presentation/src/main/java/com/example/bj_isfp_android/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/bj_isfp_android/ui/MainActivity.kt
@@ -3,76 +3,27 @@ package com.example.bj_isfp_android.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.bj_isfp_android.ui.post.PostFragment
+import com.example.bj_isfp_android.ui.theme.BJ_ISFP_AndroidTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            val navController = rememberNavController()
-
-            NavHost(
-                navController = navController,
-                startDestination = "first"
-            ){
-                composable("first") {
-                    PostFragment(navController)
-                }
-                composable("second") {
-                    SecondScreen(navController)
-                }
-                composable("third/{value}") { backStackEntry ->
-                    ThirdScreen(
-                        value = backStackEntry.arguments?.getString("value") ?: "",
-                        navController)
+            BJ_ISFP_AndroidTheme() {
+                Surface(color = MaterialTheme.colors.background) {
+                    IsFpApp()
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun SecondScreen(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "second")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            navController.navigateUp()
-        }) {
-            Text("back")
-        }
-    }
-}
-
-@Composable
-fun ThirdScreen(value: String, navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "third")
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(value)
-        Button(onClick = {
-            navController.navigateUp()
-        }) {
-            Text("back")
         }
     }
 }
