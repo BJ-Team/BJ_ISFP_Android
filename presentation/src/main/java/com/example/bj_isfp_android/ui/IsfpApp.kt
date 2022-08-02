bafa65ccca4482365b3ae6236f907df79e05481f
유현명 <aahspringaa4@naver.com>
2022-08-02 21:51:59 +0900

    ⚡️ :: Compose Main Method

--- presentation/src/main/java/com/example/bj_isfp_android/ui/IsfpApp.kt
+++ presentation/src/main/java/com/example/bj_isfp_android/ui/IsfpApp.kt
@@ -2,12 +2,26 @@ package com.example.bj_isfp_android.ui
 
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.bj_isfp_android.features.auth.LoginScreen
+import com.google.accompanist.systemuicontroller.SystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun IsFpApp() {
     val navController = rememberNavController()
     val scaffoldState = rememberScaffoldState()
+    val systemUiController = rememberSystemUiController()
 
-
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen()
+        }
+    }
 }
