7671f522cb0be2855a822e2c2fb843cca9d16919
유현명 <aahspringaa4@naver.com>
2022-08-03 19:36:16 +0900

    ⚡️ :: UserApi Method

--- data/src/main/java/com/example/data/remote/api/UserApi.kt
+++ data/src/main/java/com/example/data/remote/api/UserApi.kt
@@ -1,4 +1,23 @@
 package com.example.data.remote.api
 
+import retrofit2.http.POST
+import com.example.data.remote.url.IsFpApiUrl
+import retrofit2.http.PATCH
+import retrofit2.http.Path
+
 interface UserApi {
+    @POST(IsFpApiUrl.User.login)
+    suspend fun login()
+
+    @POST(IsFpApiUrl.User.register)
+    suspend fun register()
+
+    @POST(IsFpApiUrl.User.nameOverlapCheck)
+    suspend fun nameOverlapCheck()
+
+    @PATCH(IsFpApiUrl.User.patchPassword)
+    suspend fun patchPassword()
+
+    @PATCH(IsFpApiUrl.User.patchMyLocation)
+    suspend fun patchMyLocation()
 }
