4f1c7e53f2637ce6f183bd71a9ae83dc92ebd7a0
유현명 <aahspringaa4@naver.com>
2022-08-03 19:36:03 +0900

    ⚡️ :: Api URl Seperate

---
+++ data/src/main/java/com/example/data/remote/url/IsFpApiUrl.kt
@@ -0,0 +1,11 @@
+package com.example.data.remote.url
+
+object IsFpApiUrl {
+    object User {
+        const val login = "user/token"
+        const val register = "user/register"
+        const val nameOverlapCheck = "user/name"
+        const val patchMyLocation = "user/Place"
+        const val patchPassword = "user/password"
+    }
+}
