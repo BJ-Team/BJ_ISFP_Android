a6909686bd5873ec42f506d3f9483ccdfc10ab19
유현명 <aahspringaa4@naver.com>
2022-08-05 00:55:10 +0900

    ⚡️ :: DailyLifePostResponse Data Class

---
+++ data/src/main/java/com/example/data/remote/response/dailylife/FetchDailyLifePostResponse.kt
@@ -0,0 +1,15 @@
+package com.example.data.remote.response.dailylife
+
+import com.google.gson.annotations.SerializedName
+
+data class FetchDailyLifePostResponse(
+    @SerializedName("life_list") val lifeList: LifeList
+) {
+    data class LifeList(
+        @SerializedName("title") val title: String,
+        @SerializedName("content") val content: String,
+        @SerializedName("create_time") val createTime: String,
+        @SerializedName("life_image") val lifeImage: String,
+        @SerializedName("life_id") val lifeId: Long
+    )
+}
