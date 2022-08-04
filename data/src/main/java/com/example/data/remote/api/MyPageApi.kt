ce159e52083f5ca5792e36f095fada28dd2b660a
유현명 <aahspringaa4@naver.com>
2022-08-05 00:54:04 +0900

    ⚡️ :: MyPageApi Interface Parameter & Return value

--- data/src/main/java/com/example/data/remote/api/MyPageApi.kt
+++ data/src/main/java/com/example/data/remote/api/MyPageApi.kt
@@ -1,19 +1,32 @@
 package com.example.data.remote.api
 
+import com.example.data.remote.response.mypage.FetchMyBuyListResponse
+import com.example.data.remote.response.mypage.FetchMyPageResponse
+import com.example.data.remote.response.mypage.FetchMySellListResponse
+import com.example.data.remote.response.mypage.FetchMyWishListResponse
 import com.example.data.remote.url.IsFpApiUrl
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface MyPageApi {
 
     @GET(IsFpApiUrl.MyPage.myPage)
-    suspend fun fetchMyPage()
+    suspend fun fetchMyPage(
+        @Header("Authorization") accessToken: String
+    ): FetchMyPageResponse
 
     @GET(IsFpApiUrl.MyPage.wishPost)
-    suspend fun fetchWishPost()
+    suspend fun fetchWishPost(
+        @Header("Authorization") accessToken: String
+    ): FetchMyWishListResponse
 
     @GET(IsFpApiUrl.MyPage.sellList)
-    suspend fun fetchSellList()
+    suspend fun fetchSellList(
+        @Header("Authorization") accessToken: String
+    ): FetchMyBuyListResponse
 
     @GET(IsFpApiUrl.MyPage.buyList)
-    suspend fun fetchBuyList()
+    suspend fun fetchBuyList(
+        @Header("Authorization") accessToken: String
+    ): FetchMySellListResponse
 }
