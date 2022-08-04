d893b22d7eaf12ddff729743d93c46fe31aabd23
유현명 <aahspringaa4@naver.com>
2022-08-05 00:54:23 +0900

    ⚡️ :: ProfileApi Interface Parameter & Return Value

--- data/src/main/java/com/example/data/remote/api/ProfileApi.kt
+++ data/src/main/java/com/example/data/remote/api/ProfileApi.kt
@@ -1,19 +1,32 @@
 package com.example.data.remote.api
 
+import com.example.data.remote.response.profile.FetchOtherBuyListResponse
+import com.example.data.remote.response.profile.FetchOtherProfileResponse
+import com.example.data.remote.response.profile.FetchOtherSellListResponse
+import com.example.data.remote.response.profile.FetchOtherWishListResponse
 import com.example.data.remote.url.IsFpApiUrl
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ProfileApi {
 
     @GET(IsFpApiUrl.Profile.myPage)
-    suspend fun fetchProfile()
+    suspend fun fetchProfile(
+        @Query("user_id") userId: Long
+    ): FetchOtherProfileResponse
 
     @GET(IsFpApiUrl.Profile.wishPost)
-    suspend fun fetchWishPost()
+    suspend fun fetchWishPost(
+        @Query("user_id") userId: Long
+    ): FetchOtherWishListResponse
 
     @GET(IsFpApiUrl.Profile.sellList)
-    suspend fun fetchSellList()
+    suspend fun fetchSellList(
+        @Query("user_id") userId: Long
+    ): FetchOtherSellListResponse
 
     @GET(IsFpApiUrl.Profile.buyList)
-    suspend fun fetchBuyList()
+    suspend fun fetchBuyList(
+        @Query("user_id") userId: Long
+    ): FetchOtherBuyListResponse
 }
