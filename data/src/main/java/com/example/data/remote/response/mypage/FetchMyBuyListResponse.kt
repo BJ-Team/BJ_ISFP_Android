370246563f28231e30bb30a45294fc2175e7582f
유현명 <aahspringaa4@naver.com>
2022-08-05 00:55:30 +0900

    ⚡️ :: MyBuyListResponse Data Class

---
+++ data/src/main/java/com/example/data/remote/response/mypage/FetchMyBuyListResponse.kt
@@ -0,0 +1,14 @@
+package com.example.data.remote.response.mypage
+
+import com.google.gson.annotations.SerializedName
+
+data class FetchMyBuyListResponse(
+    @SerializedName("buy_product_list") val buy_product_list: MyBuyProductList
+) {
+    data class MyBuyProductList(
+        @SerializedName("post_id") val postId: Long,
+        @SerializedName("title") val title: String,
+        @SerializedName("location") val location: String,
+        @SerializedName("post_image") val postImage: String
+    )
+}
