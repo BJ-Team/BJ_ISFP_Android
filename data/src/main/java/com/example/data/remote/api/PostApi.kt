0a50bdb220937a981fca42cf916b6745cb3ab46e
유현명 <aahspringaa4@naver.com>
2022-08-04 21:43:47 +0900

    ⚡️ :: PostApi Interface parameter & return value

--- data/src/main/java/com/example/data/remote/api/PostApi.kt
+++ data/src/main/java/com/example/data/remote/api/PostApi.kt
@@ -1,31 +1,53 @@
 package com.example.data.remote.api
 
+import com.example.data.remote.request.post.CreatePostRequest
+import com.example.data.remote.request.post.PatchPostRequest
+import com.example.data.remote.request.post.PostReportRequest
+import com.example.data.remote.response.post.FetchPostListResponse
 import com.example.data.remote.url.IsFpApiUrl
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.PATCH
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface PostApi {
 
     @GET(IsFpApiUrl.Post.post)
-    suspend fun fetchPost()
+    suspend fun fetchPost(
+        @Header("Authorization") accessToken: String
+    ): FetchPostListResponse
 
     @POST(IsFpApiUrl.Post.post)
-    suspend fun createPost()
+    suspend fun createPost(
+        @Header("Authorization") accessToken: String,
+        @Body createPostRequest: CreatePostRequest
+    )
 
     @PATCH(IsFpApiUrl.Post.editPost)
-    suspend fun patchMyPost()
+    suspend fun patchMyPost(
+        @Header("Authorization") accessToken: String,
+        @Path ("post-id") postId: Long,
+        @Body patchPostRequest: PatchPostRequest
+    )
 
     @DELETE(IsFpApiUrl.Post.editPost)
-    suspend fun deleteMyPost()
+    suspend fun deleteMyPost(
+        @Header("Authorization") accessToken: String,
+        @Path ("post-id") postId: Long
+    )
 
     @POST(IsFpApiUrl.Post.reportPost)
-    suspend fun reportPost()
+    suspend fun reportPost(
+        @Header("Authorization") accessToken: String,
+        @Body postReportRequest: PostReportRequest
+    )
 
     @POST(IsFpApiUrl.Post.wishPost)
-    suspend fun pickWishPost()
+    suspend fun pickWishPost(
+        @Header("Authorization") accessToken: String,
+        @Path ("post-id") postId: Long
+    )
 
     @DELETE(IsFpApiUrl.Post.wishPost)
-    suspend fun deleteWishPost()
+    suspend fun deleteWishPost(
+        @Header("Authorization") accessToken: String,
+        @Path ("post-id") postId: Long
+    )
 }
