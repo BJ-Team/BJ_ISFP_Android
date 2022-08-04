9fd5d05f582b544cbfad95fcbd74863fc9215a13
유현명 <aahspringaa4@naver.com>
2022-08-05 00:56:13 +0900

    ⚡️ :: MyWishListResponse Data Class

---
+++ data/src/main/java/com/example/data/remote/response/mypage/FetchMyWishListResponse.kt
@@ -0,0 +1,15 @@
+package com.example.data.remote.response.mypage
+
+import com.google.gson.annotations.SerializedName
+
+data class FetchMyWishListResponse(
+    @SerializedName("sell_product_list") val sellProductList: SellProductList
+) {
+    data class SellProductList(
+        @SerializedName("post_id") val postId: Long,
+        @SerializedName("title") val title: String,
+        @SerializedName("location") val location: String,
+        @SerializedName("price") val price: Int,
+        @SerializedName("post_image") val postImage: String
+    )
+}
