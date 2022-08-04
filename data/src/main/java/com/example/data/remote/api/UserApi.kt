67b92ede6519767360dbe5bd84122cbf7e9b89e4
유현명 <aahspringaa4@naver.com>
2022-08-04 21:48:43 +0900

    ⚡️ :: UserApi Interface parameter & return value

--- data/src/main/java/com/example/data/remote/api/UserApi.kt
+++ data/src/main/java/com/example/data/remote/api/UserApi.kt
@@ -1,26 +1,42 @@
 package com.example.data.remote.api
 
-import retrofit2.http.POST
+import com.example.data.remote.request.user.UserPatchPasswordRequest
+import com.example.data.remote.request.user.UserSignInRequest
+import com.example.data.remote.request.user.UserSignUpRequest
+import com.example.data.remote.response.user.UserReissueResponse
+import com.example.data.remote.response.user.UserSignInResponse
 import com.example.data.remote.url.IsFpApiUrl
-import retrofit2.http.PATCH
-import retrofit2.http.Path
+import com.google.gson.annotations.SerializedName
+import retrofit2.http.*
 
 interface UserApi {
     @POST(IsFpApiUrl.User.login)
-    suspend fun login()
+    suspend fun login(
+        @Body userSignInRequest: UserSignInRequest
+    ): UserSignInResponse
 
     @POST(IsFpApiUrl.User.register)
-    suspend fun register()
+    suspend fun register(
+        @Body userSignUpRequest: UserSignUpRequest
+    )
 
     @POST(IsFpApiUrl.User.nameOverlapCheck)
-    suspend fun nameOverlapCheck()
+    suspend fun nameOverlapCheck(
+        @Query("name") name: String
+    )
 
     @PATCH(IsFpApiUrl.User.patchPassword)
-    suspend fun patchPassword()
+    suspend fun patchPassword(
+        @Body userPatchPasswordRequest: UserPatchPasswordRequest
+    )
 
     @PATCH(IsFpApiUrl.User.patchMyLocation)
-    suspend fun patchMyLocation()
+    suspend fun patchMyLocation(
+        @Body place: String
+    )
 
     @POST(IsFpApiUrl.Auth.refreshToken)
-    suspend fun refreshToken()
+    suspend fun refreshToken(
+        @Header("refresh_token") refreshToken: String
+    ): UserReissueResponse
 }
