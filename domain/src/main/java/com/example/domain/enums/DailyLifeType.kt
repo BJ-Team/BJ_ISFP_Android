ad24fa46c0473b342a5e75c206b508dcb60dba7d
유현명 <aahspringaa4@naver.com>
2022-08-05 00:57:37 +0900

    ⚡️ :: DailyLifeType Enum

---
+++ domain/src/main/java/com/example/domain/enums/DailyLifeType.kt
@@ -0,0 +1,13 @@
+package com.example.domain.enums
+
+enum class DailyLifeType {
+    QUESTION,
+    ACCIDENT,
+    RESTAURANT,
+    NEWS,
+    HOBBIES,
+    LOST,
+    PLEASE,
+    DAILY,
+    PHOTO
+}
