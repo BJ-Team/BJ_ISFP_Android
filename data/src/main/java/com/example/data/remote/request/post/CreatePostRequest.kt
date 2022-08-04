95b927f38e782d14e9ca3111f98acf99172e60f3
유현명 <aahspringaa4@naver.com>
2022-08-04 21:48:57 +0900

    ⚡️ :: CreatePostRequest

---
+++ data/src/main/java/com/example/data/remote/request/post/CreatePostRequest.kt
@@ -0,0 +1,12 @@
+package com.example.data.remote.request.post
+
+import com.example.domain.enums.PostCategoryType
+import com.google.gson.annotations.SerializedName
+
+data class CreatePostRequest(
+    @SerializedName("title") val title: String,
+    @SerializedName("content") val content: String,
+    @SerializedName("category") val category: PostCategoryType,
+    @SerializedName("price") val price: Int,
+    @SerializedName("post_image") val postImage: String
+)
