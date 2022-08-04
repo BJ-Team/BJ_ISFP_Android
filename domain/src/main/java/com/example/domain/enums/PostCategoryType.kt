838f6d073dfac8f2b2cf4ba4f42069a8593c58e4
유현명 <aahspringaa4@naver.com>
2022-08-04 21:51:58 +0900

    ⚡️ :: PostCategoryType Enum Class

---
+++ domain/src/main/java/com/example/domain/enums/PostCategoryType.kt
@@ -0,0 +1,15 @@
+package com.example.domain.enums
+
+enum class PostCategoryType {
+    DIGITAL,
+    LIFE,
+    CHILD,
+    SPORTS,
+    GIFT_CARD,
+    STUFF,
+    GAME,
+    BOOK,
+    PLANT,
+    ETC,
+    PURCHASE
+}
