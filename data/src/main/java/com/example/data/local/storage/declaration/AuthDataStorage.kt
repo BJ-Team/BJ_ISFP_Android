9106d060ace3d8f0ef2181531e14f6b470e633d2
유현명 <aahspringaa4@naver.com>
2022-08-05 13:22:57 +0900

    ⚡️ :: Token & expiredAt manage Interface

--- data/src/main/java/com/example/data/local/storage/declaration/AuthDataStorage.kt
+++ data/src/main/java/com/example/data/local/storage/declaration/AuthDataStorage.kt
@@ -1,4 +1,17 @@
 package com.example.data.local.storage.declaration
 
+import java.time.LocalDateTime
+
 interface AuthDataStorage {
+
+    fun setAccessToken(token: String)
+    fun fetchAccessToken(): String
+    fun clearAccessToken()
+
+    fun setRefreshToken(token: String)
+    fun fetchRefreshToken(): String
+    fun clearRefreshToken()
+
+    fun setExpiredAt(localDateTime: String)
+    fun fetchExpiredAt(): LocalDateTime
 }
