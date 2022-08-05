f49a6a266b518b95e64bd29d7981dca4b33e56d1
유현명 <aahspringaa4@naver.com>
2022-08-05 13:24:06 +0900

    ⚡️ :: Basic Exeption

---
+++ domain/src/main/java/com/example/domain/exeption/Basic.kt
@@ -0,0 +1,19 @@
+package com.example.domain.exeption
+
+class BadRequestException : RuntimeException()
+
+class UnauthorizedException : RuntimeException()
+
+class ForbiddenException : RuntimeException()
+
+class NotFoundException : RuntimeException()
+
+class ConflictException : RuntimeException()
+
+class NoInternetException : RuntimeException()
+
+class TimeoutException : RuntimeException()
+
+class ServerException : RuntimeException()
+
+class UnknownException : RuntimeException()
