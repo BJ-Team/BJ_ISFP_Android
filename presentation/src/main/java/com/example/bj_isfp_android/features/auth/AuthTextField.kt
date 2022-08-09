32ce1cb7cc19165433dfcc0cfe5e2360afdc78fa
유현명 <aahspringaa4@naver.com>
2022-08-09 15:16:52 +0900

    ⚡️ :: import visibility

--- presentation/src/main/java/com/example/bj_isfp_android/features/auth/AuthTextField.kt
+++ presentation/src/main/java/com/example/bj_isfp_android/features/auth/AuthTextField.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
