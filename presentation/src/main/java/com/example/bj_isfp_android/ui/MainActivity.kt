d7e7249e3a73ceb9666743ab13a5ee04ddc992cd
유현명 <aahspringaa4@naver.com>
2022-08-22 12:59:09 +0900

    ⚡️ :: MainActivity AndroidEntryPoint

--- presentation/src/main/java/com/example/bj_isfp_android/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/bj_isfp_android/ui/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.bj_isfp_android.ui.theme.BJ_ISFP_AndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
