e71782e12a285d862c43760b235668e82d10be0d
유현명 <aahspringaa4@naver.com>
2022-08-22 20:14:06 +0900

    ⚡️ :: LoginUseCase

---
+++ domain/src/main/java/com/example/domain/usecase/auth/LoginUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.domain.usecase.auth
+
+import com.example.domain.param.LoginParam
+import com.example.domain.respository.UserRepository
+import com.example.domain.usecase.UseCase
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+    private val userRepository: UserRepository
+): UseCase<LoginParam, Unit>() {
+    override suspend fun execute(data: LoginParam) {
+        userRepository.login(data)
+    }
+}
