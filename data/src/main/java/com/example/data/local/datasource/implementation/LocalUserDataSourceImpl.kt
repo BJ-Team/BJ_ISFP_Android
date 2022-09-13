0f5018da5ab2e1a535dd701f6683d4aba2171c11
유현명 <aahspringaa4@naver.com>
2022-09-13 09:24:50 +0900

    ⚡️ :: LocalUserDataSource Implementation

--- data/src/main/java/com/example/data/local/datasource/implementation/LocalUserDataSourceImpl.kt
+++ data/src/main/java/com/example/data/local/datasource/implementation/LocalUserDataSourceImpl.kt
@@ -1,4 +1,40 @@
 package com.example.data.local.datasource.implementation
 
-class LocalUserDataSourceImpl {
+import com.example.data.local.datasource.declaration.LocalUserDataSource
+import com.example.data.local.storage.declaration.AuthDataStorage
+import java.time.LocalDateTime
+import javax.inject.Inject
+
+class LocalUserDataSourceImpl @Inject constructor(
+    private val authDataStorage: AuthDataStorage
+): LocalUserDataSource {
+    override suspend fun setAccessToken(token: String) {
+        authDataStorage.setAccessToken(token)
+    }
+
+    override suspend fun fetchAccessToken(): String =
+        authDataStorage.fetchAccessToken()
+
+    override suspend fun clearAccessToken() {
+        authDataStorage.clearAccessToken()
+    }
+
+    override suspend fun setRefreshToken(token: String) {
+        authDataStorage.setRefreshToken(token)
+    }
+
+    override suspend fun fetchRefreshToken(): String =
+        authDataStorage.fetchRefreshToken()
+
+    override suspend fun clearRefreshToken() {
+        authDataStorage.clearRefreshToken()
+    }
+
+    override suspend fun setExpiredAt(localDateTime: String) {
+        authDataStorage.setExpiredAt(localDateTime)
+    }
+
+    override suspend fun fetchExpiredAt(): LocalDateTime =
+        authDataStorage.fetchExpiredAt()
+
 }
