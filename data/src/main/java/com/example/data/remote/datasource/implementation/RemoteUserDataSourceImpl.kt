4046e7dd635e203feae07b31b7559c427b631506
유현명 <aahspringaa4@naver.com>
2022-09-13 09:34:32 +0900

    ⚡️ :: All Remote Impl

--- data/src/main/java/com/example/data/remote/datasource/implementation/RemoteUserDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/datasource/implementation/RemoteUserDataSourceImpl.kt
@@ -2,7 +2,10 @@ package com.example.data.remote.datasource.implementation
 
 import com.example.data.remote.api.UserApi
 import com.example.data.remote.datasource.declaration.RemoteUserDataSource
+import com.example.data.remote.request.user.UserPatchPasswordRequest
 import com.example.data.remote.request.user.UserSignInRequest
+import com.example.data.remote.request.user.UserSignUpRequest
+import com.example.data.remote.response.user.UserReissueResponse
 import com.example.data.remote.response.user.UserSignInResponse
 import com.example.data.util.HttpHandler
 import javax.inject.Inject
@@ -14,5 +17,36 @@ class RemoteUserDataSourceImpl @Inject constructor(
         userSignInRequest: UserSignInRequest
     ) = HttpHandler<UserSignInResponse>()
         .httpRequest { userApi.login(userSignInRequest) }
+        .sendRequest()
+
+    override suspend fun postUserSignUp(
+        userSignUpRequest: UserSignUpRequest
+    ) = HttpHandler<Unit>()
+        .httpRequest { userApi.register(userSignUpRequest) }
+        .sendRequest()
+
+    override suspend fun nameOverlapCheck(
+        name: String
+    )= HttpHandler<Unit>()
+        .httpRequest { userApi.nameOverlapCheck(name) }
+        .sendRequest()
+
+    override suspend fun patchPassWord(
+        userPatchPasswordRequest: UserPatchPasswordRequest
+    )= HttpHandler<Unit>()
+        .httpRequest { userApi.patchPassword(userPatchPasswordRequest) }
+        .sendRequest()
+
+    override suspend fun patchMyLocation(
+        location: String
+    )= HttpHandler<Unit>()
+        .httpRequest { userApi.patchMyLocation(location) }
+        .sendRequest()
+
+
+    override suspend fun refreshToken(
+        token: String
+    )= HttpHandler<UserReissueResponse>()
+        .httpRequest { userApi.refreshToken(token) }
         .sendRequest()
 }
