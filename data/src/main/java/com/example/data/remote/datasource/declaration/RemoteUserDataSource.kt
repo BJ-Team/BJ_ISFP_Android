c8b3964a6f12b26342cb36d5fd5d8384efd4160c
유현명 <aahspringaa4@naver.com>
2022-09-13 09:25:17 +0900

    ⚡️ :: RemoteUserDataSource Declaration

--- data/src/main/java/com/example/data/remote/datasource/declaration/RemoteUserDataSource.kt
+++ data/src/main/java/com/example/data/remote/datasource/declaration/RemoteUserDataSource.kt
@@ -1,4 +1,10 @@
 package com.example.data.remote.datasource.declaration
 
+import com.example.data.remote.request.user.UserSignInRequest
+import com.example.data.remote.response.user.UserSignInResponse
+
 interface RemoteUserDataSource {
+    suspend fun postUserSignIn(
+        userSignInRequest: UserSignInRequest
+    ): UserSignInResponse
 }
