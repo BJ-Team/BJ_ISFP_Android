3ae60b064311c99099e43b30b7ef2d433c210151
유현명 <aahspringaa4@naver.com>
2022-09-13 09:34:20 +0900

    ⚡️ :: RemoteUserDataSource ALL Declaration

--- data/src/main/java/com/example/data/remote/datasource/declaration/RemoteUserDataSource.kt
+++ data/src/main/java/com/example/data/remote/datasource/declaration/RemoteUserDataSource.kt
@@ -1,10 +1,21 @@
 package com.example.data.remote.datasource.declaration
 
+import com.example.data.remote.request.user.UserPatchPasswordRequest
 import com.example.data.remote.request.user.UserSignInRequest
+import com.example.data.remote.request.user.UserSignUpRequest
+import com.example.data.remote.response.user.UserReissueResponse
 import com.example.data.remote.response.user.UserSignInResponse
 
 interface RemoteUserDataSource {
-    suspend fun postUserSignIn(
-        userSignInRequest: UserSignInRequest
-    ): UserSignInResponse
+    suspend fun postUserSignIn(userSignInRequest: UserSignInRequest): UserSignInResponse
+
+    suspend fun postUserSignUp(userSignUpRequest: UserSignUpRequest)
+
+    suspend fun nameOverlapCheck(name: String)
+
+    suspend fun patchPassWord(userPatchPasswordRequest: UserPatchPasswordRequest)
+
+    suspend fun patchMyLocation(location: String)
+
+    suspend fun refreshToken(token: String): UserReissueResponse
 }
