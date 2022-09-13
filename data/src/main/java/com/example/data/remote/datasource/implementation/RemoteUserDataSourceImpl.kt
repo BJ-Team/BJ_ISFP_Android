8a079127fd428bf3c923cd240f9d17b3a045e087
유현명 <aahspringaa4@naver.com>
2022-09-13 09:25:33 +0900

    ⚡️ :: RemoteUserDataSource Implementation

--- data/src/main/java/com/example/data/remote/datasource/implementation/RemoteUserDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/datasource/implementation/RemoteUserDataSourceImpl.kt
@@ -1,4 +1,18 @@
 package com.example.data.remote.datasource.implementation
 
-class RemoteUserDataSourceImpl {
+import com.example.data.remote.api.UserApi
+import com.example.data.remote.datasource.declaration.RemoteUserDataSource
+import com.example.data.remote.request.user.UserSignInRequest
+import com.example.data.remote.response.user.UserSignInResponse
+import com.example.data.util.HttpHandler
+import javax.inject.Inject
+
+class RemoteUserDataSourceImpl @Inject constructor(
+    private val userApi: UserApi
+): RemoteUserDataSource {
+    override suspend fun postUserSignIn(
+        userSignInRequest: UserSignInRequest
+    ) = HttpHandler<UserSignInResponse>()
+        .httpRequest { userApi.login(userSignInRequest) }
+        .sendRequest()
 }
