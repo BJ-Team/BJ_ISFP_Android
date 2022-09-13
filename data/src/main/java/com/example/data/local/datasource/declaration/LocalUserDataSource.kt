4e615d0ee02b3b19a6b19e6e16052106a3bfd2d9
유현명 <aahspringaa4@naver.com>
2022-09-13 09:24:19 +0900

    ⚡️ :: LocalUserDataSource Daclaration

--- data/src/main/java/com/example/data/local/datasource/declaration/LocalUserDataSource.kt
+++ data/src/main/java/com/example/data/local/datasource/declaration/LocalUserDataSource.kt
@@ -1,4 +1,17 @@
 package com.example.data.local.datasource.declaration
 
+import java.time.LocalDateTime
+
 interface LocalUserDataSource {
+
+    suspend fun setAccessToken(token: String)
+    suspend fun fetchAccessToken(): String
+    suspend fun clearAccessToken()
+
+    suspend fun setRefreshToken(token: String)
+    suspend fun fetchRefreshToken(): String
+    suspend fun clearRefreshToken()
+
+    suspend fun setExpiredAt(localDateTime: String)
+    suspend fun fetchExpiredAt(): LocalDateTime
 }
