1150d5bc9b46da77ede02e64b944cf00c2bec3aa
유현명 <aahspringaa4@naver.com>
2022-09-14 11:28:57 +0900

    ⚡️ :: DailyLife List<> & PK move TOP

--- domain/src/main/java/com/example/domain/entity/dailylife/FetchDailyLifePostEntity.kt
+++ domain/src/main/java/com/example/domain/entity/dailylife/FetchDailyLifePostEntity.kt
@@ -2,13 +2,13 @@ package com.example.domain.entity.dailylife
 
 
 data class FetchDailyLifePostEntity(
-    val lifeList: LifeList
+    val lifeList: List<LifeList>
 ) {
     data class LifeList(
+        val lifeId: Long,
         val title: String,
         val content: String,
         val createTime: String,
-        val lifeImage: String,
-        val lifeId: Long
+        val lifeImage: String
     )
 }
