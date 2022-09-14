974de69c137d7e72777b0b2f42136349a7a3ce6e
유현명 <aahspringaa4@naver.com>
2022-09-14 11:32:23 +0900

    ⚡️ :: Local DL DECLAR

--- data/src/main/java/com/example/data/local/datasource/declaration/LocalDailyLifeDataSource.kt
+++ data/src/main/java/com/example/data/local/datasource/declaration/LocalDailyLifeDataSource.kt
@@ -1,4 +1,10 @@
 package com.example.data.local.datasource.declaration
 
+import com.example.domain.entity.dailylife.FetchDailyLifePostEntity
+
 interface LocalDailyLifeDataSource {
+
+    suspend fun fetchDailyLifeList(): FetchDailyLifePostEntity
+
+    suspend fun saveDailyLifeList(list: FetchDailyLifePostEntity)
 }
