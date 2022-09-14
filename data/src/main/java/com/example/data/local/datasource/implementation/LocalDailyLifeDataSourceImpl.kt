45865fd8b629d83b855b43ddb8f66c86b1026a5c
유현명 <aahspringaa4@naver.com>
2022-09-14 11:31:56 +0900

    ⚡️ :: Local DL IMPL

--- data/src/main/java/com/example/data/local/datasource/implementation/LocalDailyLifeDataSourceImpl.kt
+++ data/src/main/java/com/example/data/local/datasource/implementation/LocalDailyLifeDataSourceImpl.kt
@@ -2,11 +2,19 @@ package com.example.data.local.datasource.implementation
 
 import com.example.data.local.dao.DailyLifeDao
 import com.example.data.local.datasource.declaration.LocalDailyLifeDataSource
+import com.example.data.local.roomentity.dailylife.toDbEntity
+import com.example.data.local.roomentity.dailylife.toEntity
 import com.example.data.local.storage.declaration.AuthDataStorage
+import com.example.domain.entity.dailylife.FetchDailyLifePostEntity
 import javax.inject.Inject
 
 class LocalDailyLifeDataSourceImpl @Inject constructor(
-    val dailyLifeDao: DailyLifeDao,
+    private val dailyLifeDao: DailyLifeDao,
     val authDataStorage: AuthDataStorage
 ): LocalDailyLifeDataSource {
+    override suspend fun fetchDailyLifeList(): FetchDailyLifePostEntity =
+        dailyLifeDao.fetchDailyLifeList().toEntity()
+
+    override suspend fun saveDailyLifeList(list: FetchDailyLifePostEntity) =
+        dailyLifeDao.saveDailyLifeList(list.toDbEntity())
 }
