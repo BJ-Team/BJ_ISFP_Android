83d7a4aa9114664a26b5c3fbcd45f538e393e8e5
유현명 <aahspringaa4@naver.com>
2022-09-14 11:34:03 +0900

    ⚡️ :: DailyLife Dao Interface

--- data/src/main/java/com/example/data/local/dao/DailyLifeDao.kt
+++ data/src/main/java/com/example/data/local/dao/DailyLifeDao.kt
@@ -1,4 +1,14 @@
 package com.example.data.local.dao
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.data.local.roomentity.dailylife.FetchDailyLifePostRoomEntity
+
 interface DailyLifeDao {
+    @Query("SELECT * FROM dailyLife")
+    suspend fun fetchDailyLifeList(): FetchDailyLifePostRoomEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveDailyLifeList(dailyLifePostRoomEntity: FetchDailyLifePostRoomEntity)
 }
