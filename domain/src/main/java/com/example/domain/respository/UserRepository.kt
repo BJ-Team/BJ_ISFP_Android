33c2686904af39a0683a833d874742d799c69e33
유현명 <aahspringaa4@naver.com>
2022-09-16 19:35:35 +0900

    ⚡️ :: UserRepository Interface

--- domain/src/main/java/com/example/domain/respository/UserRepository.kt
+++ domain/src/main/java/com/example/domain/respository/UserRepository.kt
@@ -1,7 +1,20 @@
 package com.example.domain.respository
 
-import com.example.domain.param.LoginParam
+import com.example.domain.entity.user.FetchReIssueEntity
+import com.example.domain.param.user.LoginParam
+import com.example.domain.param.user.PatchPassWordParam
+import com.example.domain.param.user.RegisterParam
 
 interface UserRepository {
-    suspend fun login(loginParam: LoginParam)
+    suspend fun signIn(loginParam: LoginParam)
+
+    suspend fun signUp(registerParam: RegisterParam)
+
+    suspend fun nameOverLapCheck(name: String)
+
+    suspend fun patchPassWord(patchPassWordParam: PatchPassWordParam)
+
+    suspend fun patchMyLocation(location: String)
+
+    suspend fun refreshToken(token: String): FetchReIssueEntity
 }
