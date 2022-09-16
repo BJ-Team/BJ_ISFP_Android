0f0219fed129d583237025814258342add315462
유현명 <aahspringaa4@naver.com>
2022-09-16 19:34:30 +0900

    ⚡️ :: DailyLifeRepository Interface

--- domain/src/main/java/com/example/domain/respository/DailyLifeRepository.kt
+++ domain/src/main/java/com/example/domain/respository/DailyLifeRepository.kt
@@ -1,4 +1,17 @@
 package com.example.domain.respository
 
+import com.example.domain.entity.dailylife.FetchDailyLifePostEntity
+import com.example.domain.enums.DailyLifeType
+import com.example.domain.param.dailylife.CreateDailyLifeParam
+import com.example.domain.param.dailylife.PatchDailyLifeParam
+
 interface DailyLifeRepository {
+    
+    suspend fun fetchDailyLifePost(dailyLifeType: DailyLifeType): FetchDailyLifePostEntity
+
+    suspend fun createDailyLifePost(createDailyLifeParam: CreateDailyLifeParam)
+
+    suspend fun patchDailyLifePost(lifeId: Long, patchDailyLifeParam: PatchDailyLifeParam)
+
+    suspend fun deleteDailyLifePost(lifeId: Long)
 }
