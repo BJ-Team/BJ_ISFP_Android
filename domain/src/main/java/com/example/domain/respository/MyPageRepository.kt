d66623c21c534eed08f14e23151b68af92f5c449
유현명 <aahspringaa4@naver.com>
2022-09-16 19:34:46 +0900

    ⚡️ :: MyPageRepository Interface

--- domain/src/main/java/com/example/domain/respository/MyPageRepository.kt
+++ domain/src/main/java/com/example/domain/respository/MyPageRepository.kt
@@ -1,4 +1,17 @@
 package com.example.domain.respository
 
+import com.example.domain.entity.mypage.FetchMyBuyListEntity
+import com.example.domain.entity.mypage.FetchMyPageEntity
+import com.example.domain.entity.mypage.FetchMySellListEntity
+import com.example.domain.entity.mypage.FetchMyWishListEntity
+
 interface MyPageRepository {
+
+    suspend fun fetchMyPage(): FetchMyPageEntity
+
+    suspend fun fetchMyWishList(): FetchMyWishListEntity
+
+    suspend fun fetchMySellList(): FetchMySellListEntity
+
+    suspend fun fetchMyBuyList(): FetchMyBuyListEntity
 }
