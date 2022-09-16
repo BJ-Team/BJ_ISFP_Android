f1d060fe54ed4dff370d96b19ae4a19d4e6998b0
유현명 <aahspringaa4@naver.com>
2022-09-16 19:35:00 +0900

    ⚡️ :: PostRepository Interface

--- domain/src/main/java/com/example/domain/respository/PostRepository.kt
+++ domain/src/main/java/com/example/domain/respository/PostRepository.kt
@@ -1,4 +1,23 @@
 package com.example.domain.respository
 
+import com.example.domain.entity.post.FetchPostListEntity
+import com.example.domain.param.post.CreatePostParam
+import com.example.domain.param.post.PatchPostParam
+import com.example.domain.param.post.PostReportParam
+
 interface PostRepository {
+
+    suspend fun fetchPost(): FetchPostListEntity
+
+    suspend fun createPost(createPostParam: CreatePostParam)
+
+    suspend fun patchPost(postId: Long, patchPostParam: PatchPostParam)
+
+    suspend fun deletePost(postId: Long)
+
+    suspend fun reportPost(postReportParam: PostReportParam)
+
+    suspend fun pickWishPost(postId: Long)
+
+    suspend fun deleteWishPost(postId: Long)
 }
