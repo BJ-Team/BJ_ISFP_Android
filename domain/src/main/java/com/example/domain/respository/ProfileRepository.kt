cfc69fa607bdc1f33900c6e21eda3feacf081727
유현명 <aahspringaa4@naver.com>
2022-09-16 19:35:20 +0900

    ⚡️ :: ProfileRepository Interface

--- domain/src/main/java/com/example/domain/respository/ProfileRepository.kt
+++ domain/src/main/java/com/example/domain/respository/ProfileRepository.kt
@@ -1,4 +1,17 @@
 package com.example.domain.respository
 
+import com.example.domain.entity.profile.FetchOtherBuyListEntity
+import com.example.domain.entity.profile.FetchOtherProfileEntity
+import com.example.domain.entity.profile.FetchOtherSellListEntity
+import com.example.domain.entity.profile.FetchOtherWishListEntity
+
 interface ProfileRepository {
+
+    suspend fun fetchProfile(userId: Long): FetchOtherProfileEntity
+
+    suspend fun fetchWishPost(userId: Long): FetchOtherWishListEntity
+
+    suspend fun fetchSellPost(userId: Long): FetchOtherSellListEntity
+
+    suspend fun fetchBuyPost(userId: Long): FetchOtherBuyListEntity
 }
