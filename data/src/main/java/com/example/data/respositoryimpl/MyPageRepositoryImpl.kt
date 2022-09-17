db7098c4fda34764928302cfadb871912c372c08
유현명 <aahspringaa4@naver.com>
2022-09-18 00:30:21 +0900

    ⚡️ :: MyPage RP Impl

--- data/src/main/java/com/example/data/respositoryimpl/MyPageRepositoryImpl.kt
+++ data/src/main/java/com/example/data/respositoryimpl/MyPageRepositoryImpl.kt
@@ -2,30 +2,46 @@ package com.example.data.respositoryimpl
 
 import com.example.data.local.datasource.declaration.LocalMyPageDataSource
 import com.example.data.remote.datasource.declaration.RemoteMyPageDataSource
+import com.example.data.remote.response.mypage.toEntity
+import com.example.data.util.OfflineCacheUtil
 import com.example.domain.entity.mypage.FetchMyBuyListEntity
 import com.example.domain.entity.mypage.FetchMyPageEntity
 import com.example.domain.entity.mypage.FetchMySellListEntity
 import com.example.domain.entity.mypage.FetchMyWishListEntity
 import com.example.domain.respository.MyPageRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class MyPageRepositoryImpl @Inject constructor(
-    val remoteMyPageDataSource: RemoteMyPageDataSource,
-    val localMyPageDataSource: LocalMyPageDataSource
+    private val remoteMyPageDataSource: RemoteMyPageDataSource,
+    private val localMyPageDataSource: LocalMyPageDataSource
 ): MyPageRepository {
-    override suspend fun fetchMyPage(): FetchMyPageEntity {
-        TODO("Not yet implemented")
-    }
 
-    override suspend fun fetchMyWishList(): FetchMyWishListEntity {
-        TODO("Not yet implemented")
-    }
+    override suspend fun fetchMyPage(): Flow<FetchMyPageEntity> =
+        OfflineCacheUtil<FetchMyPageEntity>()
+            .remoteData { remoteMyPageDataSource.fetchMyPage().toEntity() }
+            .localData { localMyPageDataSource.fetchMyPage() }
+            .doOnNeedRefresh { localMyPageDataSource.saveMyPage(it) }
+            .createFlow()
 
-    override suspend fun fetchMySellList(): FetchMySellListEntity {
-        TODO("Not yet implemented")
-    }
+    override suspend fun fetchMyWishList(): Flow<FetchMyWishListEntity> =
+        OfflineCacheUtil<FetchMyWishListEntity>()
+            .remoteData { remoteMyPageDataSource.fetchWishPost().toEntity() }
+            .localData { localMyPageDataSource.fetchMyWishList() }
+            .doOnNeedRefresh { localMyPageDataSource.saveMyWishList(it) }
+            .createFlow()
 
-    override suspend fun fetchMyBuyList(): FetchMyBuyListEntity {
-        TODO("Not yet implemented")
-    }
+    override suspend fun fetchMySellList(): Flow<FetchMySellListEntity> =
+        OfflineCacheUtil<FetchMySellListEntity>()
+            .remoteData { remoteMyPageDataSource.fetchSellList().toEntity() }
+            .localData { localMyPageDataSource.fetchMySellList() }
+            .doOnNeedRefresh { localMyPageDataSource.saveMySellList(it) }
+            .createFlow()
+
+    override suspend fun fetchMyBuyList(): Flow<FetchMyBuyListEntity> =
+        OfflineCacheUtil<FetchMyBuyListEntity>()
+            .remoteData { remoteMyPageDataSource.fetchBuyList().toEntity() }
+            .localData { localMyPageDataSource.fetchMyBuyList() }
+            .doOnNeedRefresh { localMyPageDataSource.saveMyBuyList(it) }
+            .createFlow()
 }
