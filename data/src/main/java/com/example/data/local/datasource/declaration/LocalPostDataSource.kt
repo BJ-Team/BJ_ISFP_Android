9d73e183555d2e773ef17611fb455ad679dfe2fb
유현명 <aahspringaa4@naver.com>
2022-09-18 00:12:52 +0900

    ⚡️ :: LocalPostDataSource

--- data/src/main/java/com/example/data/local/datasource/declaration/LocalPostDataSource.kt
+++ data/src/main/java/com/example/data/local/datasource/declaration/LocalPostDataSource.kt
@@ -1,4 +1,10 @@
 package com.example.data.local.datasource.declaration
 
+import com.example.domain.entity.post.FetchPostListEntity
+
 interface LocalPostDataSource {
+
+    suspend fun fetchPost(): FetchPostListEntity
+
+    suspend fun savePost(list: FetchPostListEntity)
 }
