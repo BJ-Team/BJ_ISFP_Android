fa21814a888c35b8ac7970d9301756c3b6011af7
유현명 <aahspringaa4@naver.com>
2022-09-18 00:12:42 +0900

    ⚡️ :: LocalMyPageDataSource

--- data/src/main/java/com/example/data/local/datasource/declaration/LocalMyPageDataSource.kt
+++ data/src/main/java/com/example/data/local/datasource/declaration/LocalMyPageDataSource.kt
@@ -1,4 +1,25 @@
 package com.example.data.local.datasource.declaration
 
+import com.example.domain.entity.mypage.FetchMyBuyListEntity
+import com.example.domain.entity.mypage.FetchMyPageEntity
+import com.example.domain.entity.mypage.FetchMySellListEntity
+import com.example.domain.entity.mypage.FetchMyWishListEntity
+
 interface LocalMyPageDataSource {
+
+    suspend fun fetchMyPage(): FetchMyPageEntity
+
+    suspend fun saveMyPage(fetchMyPageEntity: FetchMyPageEntity)
+
+    suspend fun fetchMyWishList(): FetchMyWishListEntity
+
+    suspend fun saveMyWishList(list: FetchMyWishListEntity)
+
+    suspend fun fetchMyBuyList(): FetchMyBuyListEntity
+
+    suspend fun saveMyBuyList(list: FetchMyBuyListEntity)
+
+    suspend fun fetchMySellList(): FetchMySellListEntity
+
+    suspend fun saveMySellList(list: FetchMySellListEntity)
 }
