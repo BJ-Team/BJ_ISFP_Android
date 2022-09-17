e831a5970437393f35cf2be866c4d6a7f7949e5e
유현명 <aahspringaa4@naver.com>
2022-09-18 01:02:44 +0900

    ⚡️ :: LocalDataSourceModule

--- presentation/src/main/java/com/example/bj_isfp_android/di/module/LocalDataSourceModule.kt
+++ presentation/src/main/java/com/example/bj_isfp_android/di/module/LocalDataSourceModule.kt
@@ -1,4 +1,41 @@
 package com.example.bj_isfp_android.di.module
 
-class LocalDataSourceModule {
+import com.example.data.local.datasource.declaration.LocalDailyLifeDataSource
+import com.example.data.local.datasource.declaration.LocalMyPageDataSource
+import com.example.data.local.datasource.declaration.LocalProfileDataSource
+import com.example.data.local.datasource.declaration.LocalUserDataSource
+import com.example.data.local.datasource.implementation.*
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocalDataSourceModule {
+
+    @Binds
+    abstract fun provideLocalUserDataSource(
+        localUserDataSourceImpl: LocalUserDataSourceImpl
+    ): LocalUserDataSource
+
+    @Binds
+    abstract fun provideLocalPostDataSource(
+        localPostDataSourceImpl: LocalPostDataSourceImpl
+    ): LocalUserDataSource
+
+    @Binds
+    abstract fun provideLocalMyPageDataSource(
+        localMyPageDataSourceImpl: LocalMyPageDataSourceImpl
+    ): LocalMyPageDataSource
+
+    @Binds
+    abstract fun provideLocalProfileDataSource(
+        localProfileDataSourceImpl: LocalProfileDataSourceImpl
+    ): LocalProfileDataSource
+
+    @Binds
+    abstract fun provideLocalDailyLifeDataSource(
+        localDailyLifeDataSourceImpl: LocalDailyLifeDataSourceImpl
+    ): LocalDailyLifeDataSource
 }
