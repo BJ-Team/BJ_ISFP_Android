6513117eaf8ab6574f6fb68984161b66b189e452
유현명 <aahspringaa4@naver.com>
2022-09-18 00:30:02 +0900

    ⚡️ :: DailyLifeRpImpl Local & Remote

--- data/src/main/java/com/example/data/respositoryimpl/DailyLifeRepositoryImpl.kt
+++ data/src/main/java/com/example/data/respositoryimpl/DailyLifeRepositoryImpl.kt
@@ -2,33 +2,58 @@ package com.example.data.respositoryimpl
 
 import com.example.data.local.datasource.declaration.LocalDailyLifeDataSource
 import com.example.data.remote.datasource.declaration.RemoteDailyLifeDataSource
+import com.example.data.remote.request.dailylife.CreateDailyLifePostRequest
+import com.example.data.remote.request.dailylife.PatchDailyLifePostRequest
+import com.example.data.remote.response.dailylife.FetchDailyLifePostResponse
+import com.example.data.remote.response.dailylife.toEntity
+import com.example.data.util.OfflineCacheUtil
 import com.example.domain.entity.dailylife.FetchDailyLifePostEntity
 import com.example.domain.enums.DailyLifeType
 import com.example.domain.param.dailylife.CreateDailyLifeParam
 import com.example.domain.param.dailylife.PatchDailyLifeParam
 import com.example.domain.respository.DailyLifeRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class DailyLifeRepositoryImpl @Inject constructor(
-    val remoteDailyLifeDataSource: RemoteDailyLifeDataSource,
-    val localDailyLifeDataSource: LocalDailyLifeDataSource
-): DailyLifeRepository {
-    override suspend fun fetchDailyLifePost(dailyLifeType: DailyLifeType): FetchDailyLifePostEntity {
-        TODO("Not yet implemented")
-    }
+    private val remoteDailyLifeDataSource: RemoteDailyLifeDataSource,
+    private val localDailyLifeDataSource: LocalDailyLifeDataSource
+) : DailyLifeRepository {
+    override suspend fun fetchDailyLifePost(dailyLifeType: DailyLifeType): Flow<FetchDailyLifePostEntity> =
+        OfflineCacheUtil<FetchDailyLifePostEntity>()
+            .remoteData {
+                remoteDailyLifeDataSource.fetchDailyLifePost(dailyLifeType).toEntity()
+            }
+            .localData { localDailyLifeDataSource.fetchDailyLifeList() }
+            .doOnNeedRefresh { localDailyLifeDataSource.saveDailyLifeList(it) }
+            .createFlow()
 
-    override suspend fun createDailyLifePost(createDailyLifeParam: CreateDailyLifeParam) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun createDailyLifePost(
+        createDailyLifeParam: CreateDailyLifeParam
+    ) = remoteDailyLifeDataSource.createDailyLifePost(createDailyLifeParam.toRequest())
 
     override suspend fun patchDailyLifePost(
         lifeId: Long,
         patchDailyLifeParam: PatchDailyLifeParam
-    ) {
-        TODO("Not yet implemented")
-    }
+    ) = remoteDailyLifeDataSource.patchDailyLifePost(lifeId, patchDailyLifeParam.toRequest())
 
-    override suspend fun deleteDailyLifePost(lifeId: Long) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteDailyLifePost(
+        lifeId: Long
+    ) = remoteDailyLifeDataSource.deleteDailyLifePost(lifeId)
+
+    private fun CreateDailyLifeParam.toRequest() =
+        CreateDailyLifePostRequest(
+            title = title,
+            content = content,
+            category = category,
+            postImage = postImage
+        )
+
+    private fun PatchDailyLifeParam.toRequest() =
+        PatchDailyLifePostRequest(
+            title = title,
+            location = title,
+            category = category,
+            postImage = postImage
+        )
 }
