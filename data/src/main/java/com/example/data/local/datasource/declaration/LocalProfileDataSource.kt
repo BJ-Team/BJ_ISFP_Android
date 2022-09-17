7793a12be98f1ea40b033fe0f60dd2bb5538d59c
유현명 <aahspringaa4@naver.com>
2022-09-18 00:13:06 +0900

    ⚡️ :: LocalProfileDataSource

--- data/src/main/java/com/example/data/local/datasource/declaration/LocalProfileDataSource.kt
+++ data/src/main/java/com/example/data/local/datasource/declaration/LocalProfileDataSource.kt
@@ -1,4 +1,24 @@
 package com.example.data.local.datasource.declaration
 
+import com.example.domain.entity.profile.FetchOtherBuyListEntity
+import com.example.domain.entity.profile.FetchOtherProfileEntity
+import com.example.domain.entity.profile.FetchOtherSellListEntity
+
 interface LocalProfileDataSource {
+
+    suspend fun fetchProfile(): FetchOtherProfileEntity
+
+    suspend fun saveProfile(list: FetchOtherProfileEntity)
+
+    suspend fun fetchWishList(): FetchOtherProfileEntity
+
+    suspend fun saveWishList(list: FetchOtherProfileEntity)
+
+    suspend fun fetchBuyList(): FetchOtherBuyListEntity
+
+    suspend fun saveBuyList(list: FetchOtherBuyListEntity)
+
+    suspend fun fetchSellList(): FetchOtherSellListEntity
+
+    suspend fun saveBUyList(list: FetchOtherSellListEntity)
 }
