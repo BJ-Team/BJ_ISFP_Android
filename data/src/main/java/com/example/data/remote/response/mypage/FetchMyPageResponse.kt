3c7a5d33ccac12aa462b66c78527843684b40eb8
유현명 <aahspringaa4@naver.com>
2022-09-18 00:28:49 +0900

    ⚡️ :: FetchMyPageResponse .toEntity Method

--- data/src/main/java/com/example/data/remote/response/mypage/FetchMyPageResponse.kt
+++ data/src/main/java/com/example/data/remote/response/mypage/FetchMyPageResponse.kt
@@ -1,5 +1,6 @@
 package com.example.data.remote.response.mypage
 
+import com.example.domain.entity.mypage.FetchMyPageEntity
 import com.google.gson.annotations.SerializedName
 
 data class FetchMyPageResponse(
@@ -7,3 +8,10 @@ data class FetchMyPageResponse(
     @SerializedName("now_my_location") val nowMyLocation: String,
     @SerializedName("user_profile") val userProfile: String
 )
+
+fun FetchMyPageResponse.toEntity() =
+    FetchMyPageEntity(
+        name = name,
+        nowMyLocation = nowMyLocation,
+        userProfile = userProfile
+    )
