3b37c6ecb9e55e80c632e0c077f0e1ca1564bf03
유현명 <aahspringaa4@naver.com>
2022-09-19 23:20:25 +0900

    ⚡️ :: Converter Type Method & Room Entity

--- data/src/main/java/com/example/data/local/roomentity/mypage/FetchMyPageRoomEntity.kt
+++ data/src/main/java/com/example/data/local/roomentity/mypage/FetchMyPageRoomEntity.kt
@@ -1,9 +1,27 @@
 package com.example.data.local.roomentity.mypage
 
-import com.google.gson.annotations.SerializedName
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.domain.entity.mypage.FetchMyPageEntity
 
+@Entity(tableName = "myPage")
 data class FetchMyPageRoomEntity(
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
     val name: String,
     val nowMyLocation: String,
     val userProfile: String
 )
+
+fun FetchMyPageRoomEntity.toEntity() =
+    FetchMyPageEntity(
+        name = name,
+        nowMyLocation = nowMyLocation,
+        userProfile = userProfile
+    )
+
+fun FetchMyPageEntity.toDbEntity() =
+    FetchMyPageRoomEntity(
+        name = name,
+        nowMyLocation = nowMyLocation,
+        userProfile = userProfile
+    )
