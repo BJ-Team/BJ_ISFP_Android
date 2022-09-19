e082b35bbd8c145a29f0d474bd8929f4de42dc45
유현명 <aahspringaa4@naver.com>
2022-09-19 23:20:54 +0900

    ⚡️ :: Converter Room Entity

--- data/src/main/java/com/example/data/local/roomentity/mypage/FetchMyWishListRoomEntity.kt
+++ data/src/main/java/com/example/data/local/roomentity/mypage/FetchMyWishListRoomEntity.kt
@@ -1,8 +1,13 @@
 package com.example.data.local.roomentity.mypage
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.domain.entity.mypage.FetchMyWishListEntity
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "wish")
 data class FetchMyWishListRoomEntity(
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
     val sellProductList: List<SellProductList>
 ) {
     data class SellProductList(
@@ -12,4 +17,32 @@ data class FetchMyWishListRoomEntity(
         val price: Int,
         val postImage: String
     )
+
+    fun SellProductList.toEntity() =
+        FetchMyWishListEntity.SellProductList(
+            postId = postId,
+            title = title,
+            location = location,
+            price = price,
+            postImage = postImage
+        )
 }
+
+fun FetchMyWishListRoomEntity.toEntity() =
+    FetchMyWishListEntity(
+        sellProductList = sellProductList.map { it.toEntity() }
+    )
+
+fun FetchMyWishListEntity.toDbEntity() =
+    FetchMyWishListRoomEntity(
+        sellProductList = sellProductList.map { it.toDbEntity() }
+    )
+
+fun FetchMyWishListEntity.SellProductList.toDbEntity() =
+    FetchMyWishListRoomEntity.SellProductList(
+        postId = postId,
+        title = title,
+        location = location,
+        price = price,
+        postImage = postImage
+    )
