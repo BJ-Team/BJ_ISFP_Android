7f29a51a7715d1bff1f1dc423247b4e07e66b047
유현명 <aahspringaa4@naver.com>
2022-09-19 23:20:04 +0900

    ⚡️ :: MyPage Room Dao

--- data/src/main/java/com/example/data/local/dao/MyPageDao.kt
+++ data/src/main/java/com/example/data/local/dao/MyPageDao.kt
@@ -1,4 +1,22 @@
 package com.example.data.local.dao
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.data.local.roomentity.mypage.FetchMyPageRoomEntity
+import com.example.data.local.roomentity.mypage.FetchMyWishListRoomEntity
+
 interface MyPageDao {
+
+    @Query("SELECT * FROM myPage")
+    suspend fun fetchMyPage(): FetchMyPageRoomEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMyPage(fetchMyPageRoomEntity: FetchMyPageRoomEntity)
+
+    @Query("SELECT * FROM wish")
+    suspend fun fetchMyWishList(): FetchMyWishListRoomEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMyWishList(fetchMyWishListRoomEntity: FetchMyWishListRoomEntity)
 }
