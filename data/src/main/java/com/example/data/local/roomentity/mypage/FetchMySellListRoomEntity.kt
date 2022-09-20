aeedc1854c05950bc4ac940ef469b0232470edff
유현명 <aahspringaa4@naver.com>
2022-09-20 09:13:36 +0900

    ⚡️ :: FetchMySellListRoomEntity Converter Method

--- data/src/main/java/com/example/data/local/roomentity/mypage/FetchMySellListRoomEntity.kt
+++ data/src/main/java/com/example/data/local/roomentity/mypage/FetchMySellListRoomEntity.kt
@@ -1,8 +1,12 @@
 package com.example.data.local.roomentity.mypage
 
-import com.google.gson.annotations.SerializedName
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.domain.entity.mypage.FetchMySellListEntity
 
+@Entity(tableName = "sell")
 data class FetchMySellListRoomEntity(
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
     val sell_product_list: List<MySellProductList>
 ) {
     data class MySellProductList(
@@ -12,4 +16,32 @@ data class FetchMySellListRoomEntity(
         val postImage: String,
         val price: Int
     )
+
+    fun MySellProductList.toEntity() =
+        FetchMySellListEntity.MySellProductList(
+            postId = postId,
+            title = title,
+            location = location,
+            postImage = postImage,
+            price = price
+        )
 }
+
+fun FetchMySellListRoomEntity.toEntity() =
+    FetchMySellListEntity(
+        sell_product_list = sell_product_list.map { it.toEntity() }
+    )
+
+fun FetchMySellListEntity.toDbEntity() =
+    FetchMySellListRoomEntity(
+        sell_product_list = sell_product_list.map { it.toDbEntity() }
+    )
+
+fun FetchMySellListEntity.MySellProductList.toDbEntity() =
+    FetchMySellListRoomEntity.MySellProductList(
+        postId = postId,
+        title = title,
+        location = location,
+        postImage = postImage,
+        price = price
+    )
