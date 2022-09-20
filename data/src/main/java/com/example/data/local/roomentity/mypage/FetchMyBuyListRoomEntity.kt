e5935b02b981788d31454350c957fd96bc4ac676
유현명 <aahspringaa4@naver.com>
2022-09-20 09:13:17 +0900

    ⚡️ :: FetchMyBuyListRoomEntity

--- data/src/main/java/com/example/data/local/roomentity/mypage/FetchMyBuyListRoomEntity.kt
+++ data/src/main/java/com/example/data/local/roomentity/mypage/FetchMyBuyListRoomEntity.kt
@@ -1,6 +1,12 @@
 package com.example.data.local.roomentity.mypage
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.domain.entity.mypage.FetchMyBuyListEntity
+
+@Entity(tableName = "buy")
 data class FetchMyBuyListRoomEntity(
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
     val buy_product_list: List<MyBuyProductList>
 ) {
     data class MyBuyProductList(
@@ -9,4 +15,30 @@ data class FetchMyBuyListRoomEntity(
         val location: String,
         val postImage: String
     )
+
+    fun MyBuyProductList.toEntity() =
+        FetchMyBuyListEntity.MyBuyProductList(
+            postId = postId,
+            title = title,
+            location = location,
+            postImage = postImage
+        )
 }
+
+fun FetchMyBuyListRoomEntity.toEntity() =
+    FetchMyBuyListEntity(
+        buy_product_list = buy_product_list.map { it.toEntity() }
+    )
+
+fun FetchMyBuyListEntity.toDbEntity() =
+    FetchMyBuyListRoomEntity(
+        buy_product_list = buy_product_list.map { it.toDbEntity() }
+    )
+
+fun FetchMyBuyListEntity.MyBuyProductList.toDbEntity() =
+    FetchMyBuyListRoomEntity.MyBuyProductList(
+        postId = postId,
+        title = title,
+        location = location,
+        postImage = postImage
+    )
