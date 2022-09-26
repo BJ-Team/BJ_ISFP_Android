98e41b93e7abed0fc8dfdf267d392c081c5641b1
유현명 <aahspringaa4@naver.com>
2022-09-26 09:21:50 +0900

    ⚡️ :: Local Post DatSource Implement

--- data/src/main/java/com/example/data/local/datasource/implementation/LocalPostDataSourceImpl.kt
+++ data/src/main/java/com/example/data/local/datasource/implementation/LocalPostDataSourceImpl.kt
@@ -2,17 +2,17 @@ package com.example.data.local.datasource.implementation
 
 import com.example.data.local.dao.PostDao
 import com.example.data.local.datasource.declaration.LocalPostDataSource
+import com.example.data.local.roomentity.post.toDbEntity
+import com.example.data.local.roomentity.post.toEntity
 import com.example.domain.entity.post.FetchPostListEntity
 import javax.inject.Inject
 
 class LocalPostDataSourceImpl @Inject constructor(
-    val postDao: PostDao
+    private val postDao: PostDao
 ): LocalPostDataSource {
-    override suspend fun fetchPost(): FetchPostListEntity {
-        TODO("Not yet implemented")
-    }
+    override suspend fun fetchPost(): FetchPostListEntity =
+        postDao.fetchPost().toEntity()
 
-    override suspend fun savePost(list: FetchPostListEntity) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun savePost(list: FetchPostListEntity) =
+        postDao.savePost(list.toDbEntity())
 }
