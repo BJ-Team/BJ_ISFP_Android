0d97a546e1aaea136a090f0a928f6d527f91cbaf
유현명 <aahspringaa4@naver.com>
2022-09-26 09:22:25 +0900

    ⚡️ :: FetchPostListRoomEntity

--- data/src/main/java/com/example/data/local/roomentity/post/FetchPostListRoomEntity.kt
+++ data/src/main/java/com/example/data/local/roomentity/post/FetchPostListRoomEntity.kt
@@ -1,6 +1,12 @@
 package com.example.data.local.roomentity.post
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.domain.entity.post.FetchPostListEntity
+
+@Entity(tableName = "post")
 data class FetchPostListRoomEntity(
+    @PrimaryKey(autoGenerate = true) var id: Int = 0,
     val nowMyLocation: String,
     val postList: List<PostList>
 ) {
@@ -12,4 +18,36 @@ data class FetchPostListRoomEntity(
         val postImage: String,
         val postId: Long
     )
+
+    fun PostList.toEntity() =
+        FetchPostListEntity.PostList(
+            title = title,
+            location = location,
+            createTime = createTime,
+            price = price,
+            postImage = postImage,
+            postId = postId
+        )
 }
+
+fun FetchPostListRoomEntity.toEntity() =
+    FetchPostListEntity(
+        nowMyLocation = nowMyLocation,
+        postList = postList.map { it.toEntity() }
+    )
+
+fun FetchPostListEntity.toDbEntity() =
+    FetchPostListRoomEntity(
+        nowMyLocation = nowMyLocation,
+        postList = postList.map { it.toDbEntity() }
+    )
+
+fun FetchPostListEntity.PostList.toDbEntity() =
+    FetchPostListRoomEntity.PostList(
+        title = title,
+        location = location,
+        createTime = createTime,
+        price = price,
+        postImage = postImage,
+        postId = postId
+    )
