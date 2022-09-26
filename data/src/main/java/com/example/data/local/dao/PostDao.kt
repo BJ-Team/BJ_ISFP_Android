9c8caf11ea16b437ef761daab3a5d906a352e377
유현명 <aahspringaa4@naver.com>
2022-09-26 09:21:17 +0900

    ⚡️ :: Post Room Dao Interface

--- data/src/main/java/com/example/data/local/dao/PostDao.kt
+++ data/src/main/java/com/example/data/local/dao/PostDao.kt
@@ -1,4 +1,15 @@
 package com.example.data.local.dao
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.data.local.roomentity.post.FetchPostListRoomEntity
+
 interface PostDao {
+
+    @Query("SELECT * FROM post")
+    suspend fun fetchPost(): FetchPostListRoomEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun savePost(fetchPostListRoomEntity: FetchPostListRoomEntity)
 }
