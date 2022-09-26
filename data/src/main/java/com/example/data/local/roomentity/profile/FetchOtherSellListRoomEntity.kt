211c02df6f9fd762b6f10735187c817f2c4a5f91
유현명 <aahspringaa4@naver.com>
2022-09-26 09:22:59 +0900

    ⚡️ :: FetchOtherSellListRoomEntity Converter Method

--- data/src/main/java/com/example/data/local/roomentity/profile/FetchOtherSellListRoomEntity.kt
+++ data/src/main/java/com/example/data/local/roomentity/profile/FetchOtherSellListRoomEntity.kt
@@ -1,5 +1,6 @@
 package com.example.data.local.roomentity.profile
 
+import com.example.domain.entity.profile.FetchOtherSellListEntity
 import com.google.gson.annotations.SerializedName
 
 data class FetchOtherSellListRoomEntity(
@@ -12,4 +13,32 @@ data class FetchOtherSellListRoomEntity(
         val postImage: String,
         val price: Int
     )
+
+    fun MySellProductList.toEntity() =
+        FetchOtherSellListEntity.MySellProductList(
+            postId = postId,
+            title = title,
+            location = location,
+            postImage = postImage,
+            price = price
+        )
 }
+
+fun FetchOtherSellListRoomEntity.toEntity() =
+    FetchOtherSellListEntity(
+        sell_product_list = sell_product_list.map { it.toEntity() }
+    )
+
+fun FetchOtherSellListEntity.toDbEntity() =
+    FetchOtherSellListRoomEntity(
+        sell_product_list = sell_product_list.map { it.toDbEntity() }
+    )
+
+fun FetchOtherSellListEntity.MySellProductList.toDbEntity() =
+    FetchOtherSellListRoomEntity.MySellProductList(
+        postId = postId,
+        title = title,
+        location = location,
+        postImage = postImage,
+        price = price
+    )
