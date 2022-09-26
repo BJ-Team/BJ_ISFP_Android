a379549dcbecee5d92de32319eb3f1854adefa68
유현명 <aahspringaa4@naver.com>
2022-09-26 19:19:45 +0900

    ⚡️ :: ISFP DataBase

--- data/src/main/java/com/example/data/local/database/ISFPDataBase.kt
+++ data/src/main/java/com/example/data/local/database/ISFPDataBase.kt
@@ -1,2 +1,45 @@
 package com.example.data.local.database
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.example.data.local.converter.DailyLifeListTypeConverter
+import com.example.data.local.dao.DailyLifeDao
+import com.example.data.local.dao.MyPageDao
+import com.example.data.local.dao.PostDao
+import com.example.data.local.dao.ProfileDao
+import com.example.data.local.roomentity.dailylife.FetchDailyLifePostRoomEntity
+import com.example.data.local.roomentity.mypage.FetchMyBuyListRoomEntity
+import com.example.data.local.roomentity.mypage.FetchMySellListRoomEntity
+import com.example.data.local.roomentity.mypage.FetchMyWishListRoomEntity
+import com.example.data.local.roomentity.post.FetchPostListRoomEntity
+import com.example.data.local.roomentity.profile.FetchOtherBuyListRoomEntity
+import com.example.data.local.roomentity.profile.FetchOtherSellListRoomEntity
+import com.example.data.local.roomentity.profile.FetchOtherWishListRoomEntity
+
+@Database(
+    entities = [
+        FetchDailyLifePostRoomEntity::class,
+        FetchMyBuyListRoomEntity::class,
+        FetchMySellListRoomEntity::class,
+        FetchMyWishListRoomEntity::class,
+        FetchPostListRoomEntity::class,
+        FetchOtherBuyListRoomEntity::class,
+        FetchOtherSellListRoomEntity::class,
+        FetchOtherWishListRoomEntity::class
+    ], version = 1, exportSchema = false
+)
+
+@TypeConverters(
+    value = [
+        DailyLifeListTypeConverter::class
+    ]
+)
+
+abstract class ISFPDataBase : RoomDatabase() {
+    abstract fun dailyLifeDao(): DailyLifeDao
+    abstract fun myPageDao(): MyPageDao
+    abstract fun postDao(): PostDao
+    abstract fun profileDao(): ProfileDao
+}
