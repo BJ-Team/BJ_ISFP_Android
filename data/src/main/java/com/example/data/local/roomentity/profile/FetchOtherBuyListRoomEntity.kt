e4b3887fc4c6fb0c4ab309e317e5fa30961b92b2
유현명 <aahspringaa4@naver.com>
2022-09-26 09:22:42 +0900

    ⚡️ :: FetchOtherBuyListRoomEntity Converter Method

--- data/src/main/java/com/example/data/local/roomentity/profile/FetchOtherBuyListRoomEntity.kt
+++ data/src/main/java/com/example/data/local/roomentity/profile/FetchOtherBuyListRoomEntity.kt
@@ -1,5 +1,7 @@
 package com.example.data.local.roomentity.profile
 
+import com.example.domain.entity.profile.FetchOtherBuyListEntity
+
 data class FetchOtherBuyListRoomEntity(
     val buy_product_list: List<MyBuyProductList>
 ) {
@@ -9,4 +11,30 @@ data class FetchOtherBuyListRoomEntity(
         val location: String,
         val postImage: String
     )
+
+    fun MyBuyProductList.toEntity() =
+        FetchOtherBuyListEntity.MyBuyProductList(
+            postId = postId,
+            title = title,
+            location = location,
+            postImage = postImage
+        )
 }
+
+fun FetchOtherBuyListRoomEntity.toEntity() =
+    FetchOtherBuyListEntity(
+        buy_product_list = buy_product_list.map { it.toEntity() }
+    )
+
+fun FetchOtherBuyListEntity.toDbEntity() =
+    FetchOtherBuyListRoomEntity(
+        buy_product_list = buy_product_list.map { it.toDbEntity() }
+    )
+
+fun FetchOtherBuyListEntity.MyBuyProductList.toDbEntity() =
+    FetchOtherBuyListRoomEntity.MyBuyProductList(
+        postId = postId,
+        title = title,
+        location = location,
+        postImage = postImage
+    )
