9a3f9e90de4c87aba52c508572086537e4be398b
유현명 <aahspringaa4@naver.com>
2022-09-27 10:14:09 +0900

    ⚡️ :: PickWishPostUseCase

---
+++ domain/src/main/java/com/example/domain/usecase/post/PickWishPostUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.domain.usecase.post
+
+import com.example.domain.entity.post.FetchPostListEntity
+import com.example.domain.respository.PostRepository
+import com.example.domain.usecase.UseCase
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class PickWishPostUseCase @Inject constructor(
+    private val postRepository: PostRepository
+): UseCase<Long, Unit>() {
+    override suspend fun execute(data: Long) {
+        postRepository.pickWishPost(data)
+    }
+}
