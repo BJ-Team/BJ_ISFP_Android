29f063cb24b7247611b0a6be17eb07bffc549e3b
유현명 <aahspringaa4@naver.com>
2022-09-27 10:11:49 +0900

    ⚡️ :: ADD Value

--- domain/src/main/java/com/example/domain/param/dailylife/PatchDailyLifeParam.kt
+++ domain/src/main/java/com/example/domain/param/dailylife/PatchDailyLifeParam.kt
@@ -3,6 +3,7 @@ package com.example.domain.param.dailylife
 import com.example.domain.enums.DailyLifeType
 
 data class PatchDailyLifeParam (
+    val lifeId: Long,
     val title: String,
     val location: String,
     val category: DailyLifeType,
