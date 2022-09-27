67609f20ec41447070f35bc1322734c3ee0b1fc9
유현명 <aahspringaa4@naver.com>
2022-09-27 09:30:53 +0900

    ⚡️ :: Add Value

--- domain/src/main/java/com/example/domain/param/post/PatchPostParam.kt
+++ domain/src/main/java/com/example/domain/param/post/PatchPostParam.kt
@@ -3,6 +3,7 @@ package com.example.domain.param.post
 import com.example.domain.enums.PostCategoryType
 
 data class PatchPostParam(
+    val postId: Long,
     val title: String,
     val content: String,
     val category: PostCategoryType,
