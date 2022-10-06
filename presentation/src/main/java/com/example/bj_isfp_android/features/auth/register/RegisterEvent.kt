e069fa9d41c53564b9ff34900a3fba36edd8464a
유현명 <aahspringaa4@naver.com>
2022-10-06 11:34:32 +0900

    ⚡️ :: RegisterEvent

---
+++ presentation/src/main/java/com/example/bj_isfp_android/features/auth/register/RegisterEvent.kt
@@ -0,0 +1,15 @@
+package com.example.bj_isfp_android.features.auth.register
+
+import com.example.bj_isfp_android.base.MviEvent
+import com.example.bj_isfp_android.features.auth.login.LoginEvent
+import com.example.domain.enums.SexType
+
+sealed class RegisterEvent: MviEvent {
+    data class InputId(val id: String): RegisterEvent()
+    data class InputPassword(val password: String): RegisterEvent()
+    data class InputName(val name: String): RegisterEvent()
+    data class InputPlace(val place: String): RegisterEvent()
+    data class InputSex(val sex: SexType): RegisterEvent()
+    object BadRequestException: RegisterEvent()
+    object NotFountException: RegisterEvent()
+}
