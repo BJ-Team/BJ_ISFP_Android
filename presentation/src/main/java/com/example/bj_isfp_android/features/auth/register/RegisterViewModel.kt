081d9fe06610a033fe2ddcfbbbe39f1cd8e038f1
유현명 <aahspringaa4@naver.com>
2022-10-07 00:20:29 +0900

    ⚡️ :: nameCheckOverlap & Register ViewModel Funtion

--- presentation/src/main/java/com/example/bj_isfp_android/features/auth/register/RegisterViewModel.kt
+++ presentation/src/main/java/com/example/bj_isfp_android/features/auth/register/RegisterViewModel.kt
@@ -1,21 +1,32 @@
 package com.example.bj_isfp_android.features.auth.register
 
 import androidx.lifecycle.Lifecycle.Event
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bj_isfp_android.base.BaseViewModel
+import com.example.bj_isfp_android.features.auth.login.LoginEvent
 import com.example.bj_isfp_android.uill.MutableEventFlow
 import com.example.bj_isfp_android.uill.asEventFlow
 import com.example.domain.enums.SexType
+import com.example.domain.exeption.BadRequestException
+import com.example.domain.exeption.ConflictException
+import com.example.domain.exeption.NotFoundException
 import com.example.domain.param.user.RegisterParam
+import com.example.domain.usecase.auth.NameCheckOverLapUseCase
 import com.example.domain.usecase.auth.RegisterUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor(
-    val registerUseCase: RegisterUseCase
+    private val registerUseCase: RegisterUseCase,
+    private val nameCheckOverLapUseCase: NameCheckOverLapUseCase
 ): BaseViewModel<RegisterState, RegisterEvent>() {
 
-    private val _eventFlow = MutableEventFlow<Event>()
+    private val _eventFlow = MutableEventFlow<RegisterEvent>()
     val eventFlow = _eventFlow.asEventFlow()
 
     val parameter =
@@ -26,6 +37,8 @@ class RegisterViewModel @Inject constructor(
             place = state.value.place,
             sex = state.value.sex
         )
+
+    val name = state.value.name
 
     fun setId(id: String) {
         sendEvent(RegisterEvent.InputId(id))
@@ -75,4 +88,47 @@ class RegisterViewModel @Inject constructor(
         }
     }
 
+    fun nameCheckOverLap() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                withContext(Dispatchers.Default) {
+                    nameCheckOverLapUseCase.execute(name)
+                }
+            }.onSuccess {
+                event(RegisterEvent.PossibleName)
+            }.onFailure {
+                when(it) {
+                    is BadRequestException -> RegisterEvent.BadRequestException
+                    is NotFoundException -> RegisterEvent.NotFountException
+                    is ConflictException -> RegisterEvent.ConflictException
+                    else -> RegisterEvent.KnownException
+                }
+            }
+        }
+    }
+
+    fun register() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                withContext(Dispatchers.Default) {
+                    registerUseCase.execute(parameter)
+                }
+            }.onSuccess {
+                event(RegisterEvent.Success)
+            }.onFailure {
+                when(it) {
+                    is BadRequestException -> RegisterEvent.BadRequestException
+                    is NotFoundException -> RegisterEvent.NotFountException
+                    is ConflictException -> RegisterEvent.ConflictException
+                    else -> RegisterEvent.KnownException
+                }
+            }
+        }
+    }
+
+    private fun event(event: RegisterEvent) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
 }
