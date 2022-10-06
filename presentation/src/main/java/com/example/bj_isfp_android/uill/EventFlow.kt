0361a18bed89a7e804d66eadf35be82b8b690fcf
유현명 <aahspringaa4@naver.com>
2022-10-07 01:19:26 +0900

    ⚡️ :: Handler Event Flow Funtion

--- presentation/src/main/java/com/example/bj_isfp_android/uill/EventFlow.kt
+++ presentation/src/main/java/com/example/bj_isfp_android/uill/EventFlow.kt
@@ -1,8 +1,16 @@
 package com.example.bj_isfp_android.uill
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicBoolean
 
 interface EventFlow<out T> : Flow<T> {
@@ -47,4 +55,17 @@ private class EventFlowSlot<T>(val value: T) {
     private val consumed: AtomicBoolean = AtomicBoolean(false)
 
     fun markConsumed(): Boolean = consumed.getAndSet(true)
+}
+
+@Composable
+inline fun <reified T> Flow<T>.observeWithLifecycle(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    noinline action: suspend(T) -> Unit
+) {
+    LaunchedEffect(key1 = Unit) {
+        lifecycleOwner.lifecycleScope.launch {
+            flowWithLifecycle(lifecycleOwner.lifecycle, minActiveState)
+        }
+    }
 }
