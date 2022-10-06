e46dd23254783b75169a5a492415b36ad9a524ae
유현명 <aahspringaa4@naver.com>
2022-10-06 11:35:28 +0900

    ⚡️ :: RegisterViewModel Input TextField Funtion

---
+++ presentation/src/main/java/com/example/bj_isfp_android/features/auth/register/RegisterViewModel.kt
@@ -0,0 +1,72 @@
+package com.example.bj_isfp_android.features.auth.register
+
+import com.example.bj_isfp_android.base.BaseViewModel
+import com.example.domain.enums.SexType
+import com.example.domain.param.user.RegisterParam
+import com.example.domain.usecase.auth.RegisterUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+    val registerUseCase: RegisterUseCase
+): BaseViewModel<RegisterState, RegisterEvent>() {
+
+    val parameter =
+        RegisterParam(
+            id = state.value.id,
+            password = state.value.password,
+            name = state.value.name,
+            place = state.value.place,
+            sex = state.value.sex
+        )
+
+    fun setId(id: String) {
+        sendEvent(RegisterEvent.InputId(id))
+    }
+
+    fun setPassword(password: String) {
+        sendEvent(RegisterEvent.InputPassword(password))
+    }
+
+    fun setName(name: String) {
+        sendEvent(RegisterEvent.InputName(name))
+    }
+
+    fun setPlace(place: String) {
+        sendEvent(RegisterEvent.InputPlace(place))
+    }
+
+    fun setSex(sex: SexType) {
+        sendEvent(RegisterEvent.InputSex(sex))
+    }
+
+    override val initialState: RegisterState
+        get() = RegisterState.initial()
+
+    override fun reduceEvent(oldState: RegisterState, event: RegisterEvent) {
+        when(event) {
+            is RegisterEvent.InputId -> {
+                setState(oldState.copy(id = event.id))
+            }
+
+            is RegisterEvent.InputPassword -> {
+                setState(oldState.copy(password = event.password))
+            }
+
+            is RegisterEvent.InputName -> {
+                setState(oldState.copy(name = event.name))
+            }
+
+            is RegisterEvent.InputPlace -> {
+                setState(oldState.copy(place = event.place))
+            }
+
+            is RegisterEvent.InputSex -> {
+                setState(oldState.copy(sex = event.sex))
+            }
+            else -> {}
+        }
+    }
+
+}
