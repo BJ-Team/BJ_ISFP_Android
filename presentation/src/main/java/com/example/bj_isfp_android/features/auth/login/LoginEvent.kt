4a6dab35b55b568b98002f91dd8182bcb9cc424e
유현명 <aahspringaa4@naver.com>
2022-10-06 15:23:57 +0900

    ⚡️ :: Success Event & KnownEvent

--- presentation/src/main/java/com/example/bj_isfp_android/features/auth/login/LoginEvent.kt
+++ presentation/src/main/java/com/example/bj_isfp_android/features/auth/login/LoginEvent.kt
@@ -3,9 +3,11 @@ package com.example.bj_isfp_android.features.auth.login
 import com.example.bj_isfp_android.base.MviEvent
 
 sealed class LoginEvent : MviEvent {
-    data class InputId(val id: String) : LoginEvent()
-    data class InputPassword(val password: String) : LoginEvent()
-    object BadRequestException : LoginEvent()
-    object NotFoundException : LoginEvent()
-    object ConflictException : LoginEvent()
+    data class InputId(val id: String): LoginEvent()
+    data class InputPassword(val password: String): LoginEvent()
+    object Success: LoginEvent()
+    object BadRequestException: LoginEvent()
+    object NotFoundException: LoginEvent()
+    object ConflictException: LoginEvent()
+    object UnKnownException: LoginEvent()
 }
